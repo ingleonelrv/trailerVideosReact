a3db0fcd0e3ee31adcbf46b2f733efdffc30d1be
ingleonelrv <ing.leonelrv@gmail.com>
2018-12-05 09:48:59 -0600

    enviando propiedades al componente y validando el tipo

--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import './media.css'
+import PropTypes from 'prop-types'
 
 class Media extends Component{
     render(){
@@ -11,16 +12,24 @@ class Media extends Component{
                 border: '1px solid red'
             }
         }
+        let { title, author, image } = this.props;
         return(
             //AQUI va el codigo JSX que no es mas que html
             <div className='Media'>
                 <div className='Media-cover'>
-                    <img className='Media-image' src='./images/covers/bitcoin.jpg' alt='' width={260} height={160} />
-                    <h3 className='Media-title'>Por que aprender React?</h3>
-                    <p className='Media-author'>Leonel Rodriguez</p>
+                    <img className='Media-image' src={image} alt='' width={260} height={160} />
+                    <h3 className='Media-title'>{title}</h3>
+                    <p className='Media-author'>{author}</p>
                 </div>                
             </div>
         )
     }
+}
+Media.propTypes = { //en minuscula porque es un atributo de Component
+    //Aqui validamos que el tipo sea el esperado, si llega otro tipo saltara un Warning
+    image : PropTypes.string,
+    title : PropTypes.string.isRequired, //Para que sea obligatorio que venga
+    author : PropTypes.string,
+    type: PropTypes.oneOf(['video','audio']) //da warning si recibe otra cosa
 }
 export default Media
