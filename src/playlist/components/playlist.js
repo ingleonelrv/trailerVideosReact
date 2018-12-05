e4b7d0433f56fdb0e2cfab36e1394e932b27bdfc
ingleonelrv <ing.leonelrv@gmail.com>
2018-12-05 16:37:10 -0600

    convirtiendo mi componente Playlist de Puro a funcional

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -2,29 +2,28 @@ import React, {Component} from 'react'
 import Media from './media'
 import './playlist.css'
 
-class Playlist extends Component{
-    render(){
-        const categories = this.props.data.categories
-        return(
-            <div>
-                {
-                    categories.map((item)=>{
-                        console.log(item)
-                        return(
-                            <div className='Playlist' key={item.id}>
-                                <h3>{item.title}</h3>
-                                {//DEBEN estar las llaves para poder escribir codigo js
-                                    item.playlist.map((item)=>{
-                                        //si no pongo el return no se imprime, el return es para hacer push a la playlist
-                                        return <Media {...item} key={item.id} /> 
-                                    })
-                                }
-                            </div>
-                        )
-                    })
-                }
-            </div>
-        )
-    }
+function Playlist(props){
+    //ya no necesita el this
+    const categories = props.data.categories
+    return(
+        <div>
+            {
+                categories.map((item)=>{
+                    console.log(item)
+                    return(
+                        <div className='Playlist' key={item.id}>
+                            <h3>{item.title}</h3>
+                            {//DEBEN estar las llaves para poder escribir codigo js
+                                item.playlist.map((item)=>{
+                                    //si no pongo el return no se imprime, el return es para hacer push a la playlist
+                                    return <Media {...item} key={item.id} /> 
+                                })
+                            }
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )  
 }
 export default Playlist
