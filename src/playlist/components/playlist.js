8daae3f1633247df8f986c694d2f4277a8d5fe3f
ingleonelrv <ing.leonelrv@gmail.com>
2018-12-05 17:48:34 -0600

    composicion de los componentes playlist>play|pause|vol|fs>icon, children

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react'
 import Media from './media'
 import './playlist.css'
+import Play from '../../icons/components/play';
+import Pause from '../../icons/components/pause';
+import Volume from '../../icons/components/volume';
+import FullScreen from '../../icons/components/full-screen';
 
 function Playlist(props){
     //ya no necesita el this
@@ -13,6 +17,11 @@ function Playlist(props){
                     return(
                         <div className='Playlist' key={item.id}>
                             <h3>{item.title}</h3>
+                            {/* Estas propiedades van hacia Play pero el que las necesita es Icon */}
+                            <Play size={50} color='green' /> 
+                            <Pause size={50} color='red' /> 
+                            <Volume size={50} color='grey' /> 
+                            <FullScreen size={50} color='black' /> 
                             {//DEBEN estar las llaves para poder escribir codigo js
                                 item.playlist.map((item)=>{
                                     //si no pongo el return no se imprime, el return es para hacer push a la playlist
