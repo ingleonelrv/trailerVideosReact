1cf84154d98f313213b978a7207201cf97b3f96c
ingleonelrv <ing.leonelrv@gmail.com>
2018-12-05 08:45:21 -0600

    agregando estilos a mi primer componente

--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -1,14 +1,23 @@
 import React, {Component} from 'react'
+import './media.css'
 
 class Media extends Component{
     render(){
+        const styles = { //objeto json
+            container:{ //key
+                color: '#44546b',
+                cursor: 'pointer',
+                width: 245,
+                border: '1px solid red'
+            }
+        }
         return(
             //AQUI va el codigo JSX que no es mas que html
-            <div>
-                <div>
-                    <img src='' alt='' width={245} height={160} />
-                    <h3>Por que aprender React?</h3>
-                    <p>Leonel Rodriguez</p>
+            <div className='Media'>
+                <div className='Media-cover'>
+                    <img className='Media-image' src='./images/covers/bitcoin.jpg' alt='' width={260} height={160} />
+                    <h3 className='Media-title'>Por que aprender React?</h3>
+                    <p className='Media-author'>Leonel Rodriguez</p>
                 </div>                
             </div>
         )
