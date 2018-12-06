061b72903ff07f3adab41cfff87da5fecac7013b
ingleonelrv <ing.leonelrv@gmail.com>
2018-12-05 19:44:31 -0600

    Componente Related, estilos de este, de Categories y Category

--- src/pages/container/home.js
+++ src/pages/container/home.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react'
 import HomeLayout from '../component/home-layout'
 import Categories from '../../categories/components/categories';
+import Related from '../component/related'
 
 class Home extends Component{
     render(){
         return(
             <HomeLayout>
+                <Related />
                 <Categories categories={this.props.data.categories} />
             </HomeLayout>
         )
