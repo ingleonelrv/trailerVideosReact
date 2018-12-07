b6c0cb0b909a3ac5ce6efae91ce90c333687cf15
ingleonelrv <ing.leonelrv@gmail.com>
2018-12-06 18:37:22 -0600

    creando la UI PlayPause

--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -2,12 +2,22 @@ import React, {Component} from 'react'
 import VideoPlayerLayout from '../components/video-player-layout'
 import Video from '../components/video'
 import Title from '../components/title'
+import PlayPause from '../components/play-pause'
 
 class VideoPlayer extends Component{
+    state={
+        pause: true
+    }
+    togglePlay = event => {
+        this.setState({
+            pause:!this.state.pause
+        })
+    }
     render(){
         return(
             <VideoPlayerLayout>
                 <Title title='Titulo del video' />
+                <PlayPause pause={this.state.pause} handleClick={this.togglePlay} />
                 <Video autoPlay={false} src='http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4' />
             </VideoPlayerLayout>
         )
