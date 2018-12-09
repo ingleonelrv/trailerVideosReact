086d4fb57fbdfe6d9d155e012fec03159fa32979
ingleonelrv <ing.leonelrv@gmail.com>
2018-12-09 10:39:41 -0600

    agregando store al proyecto Video

--- src/entries/home.js
+++ src/entries/home.js
@@ -2,7 +2,22 @@ import React from 'react'
 import {render} from 'react-dom'
 import Home from '../pages/container/home'
 import data from '../api.json'
+import {createStore} from 'redux'
 
+//Punto importante: Aqui se define el modelo de datos a tener en la app
+const initialState = {
+    data: {
+        ...data
+    }
+}
+
+const store = createStore(
+    (state)=> state,
+    initialState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+console.log(store.getState())
 
 const container = document.getElementById('home-container')
 render(<Home data={data} />, container); // Lo muestro en el navegador como un tag
