4a53af4359c4d75569c91729b6047e8c720a8053
ingleonelrv <ing.leonelrv@gmail.com>
2018-12-10 16:38:58 -0600

    Normalizando datos

--- src/entries/home.js
+++ src/entries/home.js
@@ -5,6 +5,9 @@ import data from '../api.json'
 import {createStore} from 'redux'
 import {Provider} from 'react-redux'
 import reducer from '../reducers/data'
+import normalizedData from '../schemas/index'
+
+console.log(normalizedData)
 
 //Punto importante: Aqui se define el modelo de datos a tener en la app
 const initialState = {
